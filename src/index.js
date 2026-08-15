4153079167218544d4807f970285d3615b926277
khayri <khayriramzy@gmail.com>
2026-08-15 02:02:20 -0400

    Hard-code html and then create pseudocode for the javascript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import "./styles.css";
 
 console.log('hello world');
+
+// create bonBurgerLogo img variable
+// set src to the bon burger logo
+// set width to 200
+
+// create h1 variable
+// set text content to "Bon's Burgers"
+
+// create div variables for brief description of restaurant
