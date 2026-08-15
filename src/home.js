225c34ffae90ba4203a1a09f7c786a5fedcea4e6
khayri <khayriramzy@gmail.com>
2026-08-15 02:22:53 -0400

    Create home-page in home.js and import it to index.js

---
+++ src/home.js
@@ -0,0 +1,31 @@
+import bonsBurgers from "./images/bons_burgers.jpg";
+
+export function component(){
+    const container = document.createElement('div');
+
+    // create bonBurgerLogo img variable
+    const bonBurgerLogo = document.createElement('img');
+    // set src to the bon burger logo
+    bonBurgerLogo.src = bonsBurgers;
+    // set width to 200
+    bonBurgerLogo.width = 200;
+
+    // create h1 variable
+    const header = document.createElement('h1');
+    // set text content to "Bon's Burgers"
+    header.textContent = "Bon's Burgers";
+
+    // create div variables for brief description of restaurant
+    const div1 = document.createElement('div');
+    div1.textContent = "Welcome to Bon's Burgers! A beautiful place full of joy and togetherness!";
+
+    const div2 = document.createElement('div');
+    div2.textContent = "We serve everyone here with respect and make our food with nothing but the freshest ingredients, and the most important ingredient of them all: love.";
+
+    container.appendChild(bonBurgerLogo);
+    container.appendChild(header);
+    container.appendChild(div1);
+    container.appendChild(div2);
+
+    return container;
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import "./styles.css";
+import {component} from "./home.js";
+
 
 console.log('hello world');
 
-// create bonBurgerLogo img variable
-// set src to the bon burger logo
-// set width to 200
 
-// create h1 variable
-// set text content to "Bon's Burgers"
-
-// create div variables for brief description of restaurant
+document.body.appendChild(component());
