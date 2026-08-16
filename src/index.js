10bfa49bf230482349972984af8aae3588882fce
khayri <khayriramzy@gmail.com>
2026-08-15 21:47:32 -0400

    Add event listeners to buttons to redirect to respective pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import "./styles.css";
-import {component} from "./home.js";
+import {home} from "./home.js";
+import { menu } from "./menu.js";
+import { about } from "./about.js";
 
 
 console.log('hello world');
 
-component();
+home();
+// menu();
+// about();
+
+const contentDiv = document.querySelector("div#content");
+// select home, menu, about buttons
+const homeBtn = document.querySelector("button#home");
+const menuBtn = document.querySelector("button#menu");
+const aboutBtn = document.querySelector("button#about");
+// on clicking home:
+homeBtn.addEventListener("click", () => {
+    // remove content div children
+    contentDiv.textContent = "";
+    // execute home()
+    home();
+})
+    
+
+// on clicking menu
+menuBtn.addEventListener("click", () => {
+    // remove content div children
+    contentDiv.textContent = "";
+    // execute menu()
+    menu();
+})
+    
+
+// on clicking about
+aboutBtn.addEventListener("click", () => {
+    // remove content div children
+    contentDiv.textContent = "";
+    // execute about()
+    about();
+})
+    
