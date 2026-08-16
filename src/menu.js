d326be63fc13a1417cbe72033101925868565337
khayri <khayriramzy@gmail.com>
2026-08-15 21:38:35 -0400

    Add more menu items

--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,11 @@
 
-export function menu(){
+export function menu() {
     const contentDiv = document.querySelector('div#content');
 
     // create menu header
     const menuHeader = document.createElement('h1');
     menuHeader.textContent = 'Menu';
-    
+
     // create beverages section
     const beverageContainer = document.createElement('div');
     beverageContainer.classList.add('beverage-container')
@@ -15,16 +15,16 @@ export function menu(){
 
     // beverage items contain:
     // individual container
-    const beverageItem = document.createElement('div');
+    let beverageItem = document.createElement('div');
     beverageItem.classList.add('beverage-item')
     // header3 for name of beverage
-    const beverageh3 = document.createElement('h3');
+    let beverageh3 = document.createElement('h3');
     beverageh3.textContent = 'Wonderland Smoothie';
     // div for brief description
-    const beverageDesc = document.createElement('div');
+    let beverageDesc = document.createElement('div');
     beverageDesc.textContent = 'Wonderful smoothie straight from Wonderland';
     // price
-    const beveragePrice = document.createElement('div');
+    let beveragePrice = document.createElement('div');
     beveragePrice.textContent = '$1.5';
     // img of the beverage (optional)
     // append all that to the individual container
@@ -32,11 +32,32 @@ export function menu(){
     beverageItem.appendChild(beverageDesc);
     beverageItem.appendChild(beveragePrice);
 
-    
+    beverageContainer.appendChild(beverageItem);
+
+    // beverage item 2
+    // individual container
+    beverageItem = document.createElement('div');
+    beverageItem.classList.add('beverage-item')
+    // header3 for name of beverage
+    beverageh3 = document.createElement('h3');
+    beverageh3.textContent = 'Orange Juice';
+    // div for brief description
+    beverageDesc = document.createElement('div');
+    beverageDesc.textContent = 'Fresh orange juice :3';
+    // price
+    beveragePrice = document.createElement('div');
+    beveragePrice.textContent = '$0.5';
+    // img of the beverage (optional)
+    // append all that to the individual container
+    beverageItem.appendChild(beverageh3);
+    beverageItem.appendChild(beverageDesc);
+    beverageItem.appendChild(beveragePrice);
+
+
     // append beverages to beverageContainer
     beverageContainer.appendChild(beverageItem);
 
-    
+
     // create main course section
     const maincourseContainer = document.createElement('div');
     maincourseContainer.classList.add('maincourse-container');
@@ -45,24 +66,48 @@ export function menu(){
     maincourseContainer.appendChild(maincourseHeader);
 
     // main course items contain same thing as beverages
+    // main course item 1
     // individual container
-    const maincItem = document.createElement('div');
+    let maincItem = document.createElement('div');
     maincItem.classList.add('maincourse-item')
     // header3 for name of main course
-    const mainch3 = document.createElement('h3');
+    let mainch3 = document.createElement('h3');
     mainch3.textContent = "Bon's Burger";
     // div for brief description
-    const maincDesc = document.createElement('div');
+    let maincDesc = document.createElement('div');
     maincDesc.textContent = "The famous burger made by Bon himself";
     // price
-    const maincPrice = document.createElement('div');
+    let maincPrice = document.createElement('div');
     maincPrice.textContent = '$4';
     // img of the beverage (optional)
     // append all that to the individual container
     maincItem.appendChild(mainch3);
     maincItem.appendChild(maincDesc);
     maincItem.appendChild(maincPrice);
-    
+
+    // append main courses to maincourseContainer
+    maincourseContainer.appendChild(maincItem)
+
+
+    // main course item 2
+    // individual container
+    maincItem = document.createElement('div');
+    maincItem.classList.add('maincourse-item')
+    // header3 for name of main course
+    mainch3 = document.createElement('h3');
+    mainch3.textContent = "Philly Cheesesteak";
+    // div for brief description
+    maincDesc = document.createElement('div');
+    maincDesc.textContent = "mmm philly cheesesteak so yummy";
+    // price
+    maincPrice = document.createElement('div');
+    maincPrice.textContent = '$6';
+    // img of the beverage (optional)
+    // append all that to the individual container
+    maincItem.appendChild(mainch3);
+    maincItem.appendChild(maincDesc);
+    maincItem.appendChild(maincPrice);
+
     // append main courses to maincourseContainer
     maincourseContainer.appendChild(maincItem)
 
